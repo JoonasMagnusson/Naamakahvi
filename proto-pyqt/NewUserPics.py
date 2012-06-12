2012322cc847c25c6624fd5aadc264569143fe14
Jeuro <juuuro@gmail.com>
2012-06-12 18:43:47 +0300

    Created NewUserPicsWindow class

---
+++ proto-pyqt/NewUserPics.py
@@ -0,0 +1,15 @@
+from PyQt4 import uic
+
+
+FormClass, BaseClass = uic.loadUiType("shooting.ui")
+
+
+class NewUserPicsWindow(BaseClass, FormClass):
+    def __init__(self, parent=None):
+        super(BaseClass, self).__init__(parent)
+         
+        self.setupUi(self)
+        self.show()
+
+    def backToStart(self):
+        self.close()
