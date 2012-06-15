a6e58d50e9c985f547e2372b6296d338b7abc75c
Janne Ronkonen <leadnose@gmail.com>
2012-06-15 13:55:50 +0300

    Lisätty http-client pom.xml:ään ja Client käyttämään sitä

--- Naamakahvi-Client/src/main/java/naamakahvi/naamakahviclient/Client.java
+++ Naamakahvi-Client/src/main/java/naamakahvi/naamakahviclient/Client.java
@@ -1,95 +1,120 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package naamakahvi.naamakahviclient;
+  Tämä on vasta todella karkea luonnos siitä millainen clientistä ehkä mahdollisesti saattaisi tulla,
+  ehdotuksia otetaan vastaan.
+
+  Tämän kikkareen on siis tarkoitus keskustella serverin kanssa ja tarjota eri käyttöliittymille
+  API jotta käyttöliittymien ei tarvitse välittää siitä miten serverin kanssa kommunikointi tapahtuu.
+
+  t. Janne
+*/
+
+package naamakahvi.naamakahviclient; 
 
 import java.util.List;
+import java.net.URI;
 
-/*
- * Tämä on vasta todella karkea luonnos siitä millainen clientistä ehkä
- * mahdollisesti saattaisi tulla, ehdotuksia otetaan vastaan.
- *
- * Tämän kikkareen on siis tarkoitus keskustella serverin kanssa ja tarjota eri
- * käyttöliittymille API jotta käyttöliittymien ei tarvitse välittää siitä miten
- * serverin kanssa kommunikointi tapahtuu.
- *
- * t. Janne
- */
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
 
-/*
- * Tämän ImageData-luokan olis tarkoitus olla se data mitä liikkuu clientistä
- * serverille päin ja jonka serveri sitten tallentaa jotta se voi myöhemmin
- * käyttää sitä tunnistamiseen.
- *
- * Tämän luokan toteutus riippuu aika paljon siitä mitä OpenCV:ltä päin on
- * tulossa.
- */
-class ImageData {
-}
 
-public class Client {
+
+
+public class Client 
+{
+	/*
+	  Tämän ImageData-luokan olis tarkoitus olla se data mitä liikkuu
+	  clientistä serverille päin ja jonka serveri sitten tallentaa jotta se
+	  voi myöhemmin käyttää sitä tunnistamiseen.
+
+	  Tämän luokan toteutus riippuu aika paljon siitä mitä OpenCV:ltä päin on tulossa.
+	 */
+	class ImageData {}
+	
     // Tämä User-luokka on Clientin-luokan "sisällä" jotta Client voi kutsua sen privaatti-konstruktoria.
-
     class User {
         /*
-         * Koska serveri "luo" uudet käyttäjät, tämän konstruktori on privaatti
-         * ja käyttäjiin on tarkoitus päästä käsiksi Clientin registerUser() ja
-         * authenticateUser() metodien kautta.
-         */
+          Koska serveri "luo" uudet käyttäjät, tämän konstruktori on privaatti
+          ja käyttäjiin on tarkoitus päästä käsiksi Clientin registerUser() ja authenticateUser() metodien kautta.
+        */
+        private User(String username, ImageData imagedata) {}
 
-        private User(String username, ImageData data) {
-        }
-
-        public String getUserName() {
+        public String getUserName()
+        {
             // TODO
-            return null;
-        }
-
-        public ImageData getImageData() {
-            // TODO
-            return null;
+        	throw new RuntimeException();
         }
 
         /*
-         * Varsinainen osto ja tuonti tapahtuvat näillä metodeilla
-         */
-//        public void osta(tuote, määrä) throws Exception {
-//        }
-//
-//        public void tuo(tuote, määrä) throws Exception {
-//        }
+        // Varsinainen osto ja tuonti tapahtuvat näillä metodeilla 
+        public void osta(tuote, määrä) throws JokuException
+        {
+            
+        }
+
+        public void tuo(tuote, määrä) throws JokuException
+        {
+            
+        }
+        */
+        
+        
+    }
+
+    private String host;
+    private int port;
+
+    /*
+      Konstruktori ainoastaan tallentaa hostin nimen ja portin, joita se käyttää myöhemmissä metodikutsuissaan, jotka muodostavat aina uuden HTTP-yhteyden.
+     */
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
     }
 
     /*
-     * Konstruktori ottaa yhteyttä serveriin, jonka jälkeen clienttiä voi
-     * käyttää. Tarkoitus on että jokaista osto/tuonti-tapahtumaa varten luodaan
-     * uusi Clientti.
+      Tätä on tarkoitus kutsua kun clienttiä ei enää kaivata.
      */
-    public Client(String host, int port) //throws Exception // jos ei saada yhteyttä jne. niin heitetään poikkeus
+    public void close()
     {
-        // TODO
-    }
-
-    /*
-     * Tätä on tarkoitus kutsua kun clienttiä ei enää kaivata.
-     */
-    public void close() {
         // TODO 
     }
-
-//    public User registerUser(String username, ImageData imagedata)
-//            throws Exception {
-//        // TODO
-//    }
+    
+    public User registerUser(String username, ImageData imagedata)
+        throws Exception
+    {
+        HttpClient hc = new DefaultHttpClient();
+        URIBuilder ub = new URIBuilder();
+        ub.setScheme("http").setHost(this.host).setPath("/register")
+            .setParameter("username", username)
+            .setParameter("imagedata", imagedata.toString());
+        URI uri = ub.build();
+        // HttpGet get = new HttpGet(uri);
+        System.out.println(uri);
+        return null;
+    }
 
     /*
-     * Tälle jutulle annetaan OpenCV:ltä/kameralta kuvadataa, pusketaan se
-     * serverille joka antaa tuloksena käyttäjän tai joukon käyttäjiä joita kuva
-     * vastaa.
+      Tälle jutulle annetaan OpenCV:ltä/kameralta kuvadataa, pusketaan se serverille
+      joka antaa tuloksena käyttäjän tai joukon käyttäjiä joita kuva vastaa.
      */
-//    public List<User> authenticateUser(ImageData imagedata) {
-//        // TODO 
-//        return null;
-//    }
+    public List<User> authenticateImage(ImageData imagedata)
+    {
+        // TODO 
+    	throw new RuntimeException();
+    }
+
+    public static void main(String[] args)
+    {
+        try {
+            Client c = new Client("le host", 1234);
+            c.registerUser("veikko nieminen", null);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+    
 }
