84bd292c173cf6aad9c6496f5605a311099a47f6
Ossi Vare <ossi.vare@gmail.com>
2012-06-18 15:53:09 +0300

    android näkymät + projektipohja

---
+++ Naamakahvi_Android/src/com/facecafe/app/Naamakahvi_Menu.java
@@ -0,0 +1,13 @@
+package com.facecafe.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Naamakahvi_Menu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
