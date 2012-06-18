fb3b4a2f5884be46ed0a9f5597450b8edb89e198
Eeva Terkki <juuuro@gmail.com>
2012-06-18 15:11:12 +0300

    Tekstiautentikoinnin testaus, vaiheessa

--- Naamakahvi-Client/src/test/java/naamakahvi/naamakahviclient/ClientTest.java
+++ Naamakahvi-Client/src/test/java/naamakahvi/naamakahviclient/ClientTest.java
@@ -1,6 +1,8 @@
 package naamakahvi.naamakahviclient;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import junit.framework.TestCase;
@@ -10,6 +12,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.localserver.LocalTestServer;
+import org.apache.http.params.DefaultedHttpParams;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpRequestHandler;
 import org.junit.After;
@@ -21,10 +24,13 @@ import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 public class ClientTest {
 
     private LocalTestServer server = null;
-   // @Mock
+   
+    private static HashMap<String, IUser> users = new HashMap<String, IUser>();
+    
     HttpRequestHandler register_handler = new HttpRequestHandler() {
 
-        public void handle(HttpRequest hr, HttpResponse hr1, HttpContext hc) throws HttpException, IOException {
+        public void handle(HttpRequest hr, HttpResponse hr1, HttpContext hc) throws HttpException, IOException {      
+            
             String username = "asdf";
             System.out.println("username: " + username);
             StringEntity se = new StringEntity(username, ContentType.create("text/plain", "UTF-8"));                     
@@ -33,7 +39,27 @@ public class ClientTest {
             
         }
     };
+    
+    HttpRequestHandler authenticate_text_handler = new HttpRequestHandler() {
 
+        public void handle(HttpRequest request, HttpResponse response, HttpContext hc) throws HttpException, IOException {
+            DefaultedHttpParams params = (DefaultedHttpParams) request.getParams();
+            
+            Set<String> names = params.getNames();
+            for (String s : names) {
+                System.out.println(s);
+            }
+            System.out.println("params: " + request.getParams());
+            //String username = "asdf"; //  request.getParams().getParameter("username").toString();
+            
+            //System.out.println("username: " + username);
+            StringEntity se = new StringEntity("asdf", ContentType.create("text/plain", "UTF-8"));                     
+            response.setEntity(se);
+            response.setStatusCode(200);
+            
+        }
+    };
+    
     static int port;
     static String host;
     
@@ -42,6 +68,7 @@ public class ClientTest {
         server = new LocalTestServer(null, null);
         
         server.register("/register/*", register_handler);
+        server.register("/authenticate_text/*", authenticate_text_handler);
         
         
         try {
@@ -65,7 +92,7 @@ public class ClientTest {
     }
     
     @Test
-    public void moi() {
+    public void register_test() throws Exception {
         Client c = new Client(host, port);
         try {
             IUser u = c.registerUser("asdf", null);
@@ -73,6 +100,20 @@ public class ClientTest {
             assertEquals(u.getUserName(), "asdf");
         } catch (Exception ex) {
             Logger.getLogger(ClientTest.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
+        }
+    }
+    
+    @Test
+    public void authenticate_test() throws Exception {
+        Client c = new Client(host, port);
+        try {
+            IUser u = c.authenticateText("asdf");
+            System.out.println(u.getUserName());
+            assertEquals(u.getUserName(), "asdf");
+        } catch (Exception ex) {
+            Logger.getLogger(ClientTest.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
         }
     }
 }
