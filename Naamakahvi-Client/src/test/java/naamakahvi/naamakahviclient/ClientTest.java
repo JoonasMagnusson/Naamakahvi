03790c5b6d589d5d033f2ffe2a2354988ceb5d85
Eeva Terkki <juuuro@gmail.com>
2012-06-21 11:46:17 +0300

    Clientin testit toimii taas

--- Naamakahvi-Client/src/test/java/naamakahvi/naamakahviclient/ClientTest.java
+++ Naamakahvi-Client/src/test/java/naamakahvi/naamakahviclient/ClientTest.java
@@ -1,30 +1,27 @@
 package naamakahvi.naamakahviclient;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import junit.framework.TestCase;
 import org.apache.http.*;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.localserver.LocalTestServer;
-import org.apache.http.params.DefaultedHttpParams;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpRequestHandler;
 import org.apache.http.util.EntityUtils;
 import org.junit.After;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 public class ClientTest {
 
     private LocalTestServer server = null;
     private static HashMap<String, IUser> users = new HashMap<String, IUser>();
-    HttpRequestHandler register_handler = new HttpRequestHandler() {
+    HttpRequestHandler registrationHandler = new HttpRequestHandler() {
 
         public void handle(HttpRequest request, HttpResponse response, HttpContext context) throws HttpException, IOException {
             HttpEntity entity = null;
@@ -38,13 +35,17 @@ public class ClientTest {
             } else {
                 data = EntityUtils.toByteArray(entity);
             }
-
-            String username = new String(data);
-            StringEntity stringEntity = new StringEntity(username, ContentType.create("text/plain", "UTF-8"));
+            
+            String username = new String(data).substring(9);
+            IUser user = new User(username, null);
+            StringEntity stringEntity = new StringEntity(new Gson().toJson(user, User.class), ContentType.create("text/plain", "UTF-8"));
             response.setEntity(stringEntity);
             response.setStatusCode(200);
         }
     };
+    
+    HttpRequestHandler authenticationHandler = registrationHandler;
+    
 //    HttpRequestHandler authenticate_text_handler = new HttpRequestHandler() {
 //
 //        public void handle(HttpRequest request, HttpResponse response, HttpContext hc) throws HttpException, IOException {
@@ -71,7 +72,7 @@ public class ClientTest {
     public void setUp() {
         server = new LocalTestServer(null, null);
 
-        server.register("/register/*", register_handler);
+        server.register("/register/*", registrationHandler);
         //server.register("/authenticate_text/*", authenticate_text_handler);
 
 
@@ -95,25 +96,25 @@ public class ClientTest {
     }
 
     @Test
-    public void register_test() throws Exception {
+    public void registrationTest() throws Exception {
         Client c = new Client(host, port);
         try {
-            IUser u = c.registerUser("asdf", null);
+            IUser u = c.registerUser("Pekka", null);
             System.out.println(u.getUserName());
-            assertEquals(u.getUserName(), "asdf");
+            assertEquals(u.getUserName(), "Pekka");
         } catch (Exception ex) {
             Logger.getLogger(ClientTest.class.getName()).log(Level.SEVERE, null, ex);
             throw ex;
         }
     }
 
-  //  @Test
-    public void authenticate_test() throws Exception {
+    @Test
+    public void authenticationTest() throws Exception {
         Client c = new Client(host, port);
         try {
-            IUser u = c.authenticateText("asdf");
+            IUser u = c.authenticateText("Harri");
             System.out.println(u.getUserName());
-            assertEquals(u.getUserName(), "asdf");
+            assertEquals(u.getUserName(), "Harri");
         } catch (Exception ex) {
             Logger.getLogger(ClientTest.class.getName()).log(Level.SEVERE, null, ex);
             throw ex;
