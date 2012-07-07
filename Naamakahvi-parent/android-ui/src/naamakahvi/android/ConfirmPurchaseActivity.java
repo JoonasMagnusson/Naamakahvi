0ce0148f0572ea860bc6ff1ad0910ee240c32d10
iimakis <iimakis@KUOLAPALLO.(none)>
2012-07-07 19:02:11 +0300

    ConfirmPurchaseActivity refaktorointia pienempiin metodeihin.

--- Naamakahvi-parent/android-ui/src/naamakahvi/android/ConfirmPurchaseActivity.java
+++ Naamakahvi-parent/android-ui/src/naamakahvi/android/ConfirmPurchaseActivity.java
@@ -19,19 +19,21 @@ public class ConfirmPurchaseActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.confirm_purchase);
-		
-		setSaldos("username");
+		setContentView(R.layout.confirm_purchase);		
+		setCountdown();
 
-		// here will be returned list of users from client
+		// TODO: here will be returned list of users from client
         ListView possibleUsersListView = (ListView) findViewById(R.id.possibleUsers);
         String[] testUsers = new String[] {"aapeli", "kahvikonkari", "moikkaaja", "testi"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-            	android.R.layout.simple_list_item_1, android.R.id.text1, testUsers);
-        possibleUsersListView.setAdapter(adapter);
-		
-        //User clicking a name from list results in changing into another user
-        possibleUsersListView.setOnItemClickListener(new OnItemClickListener() {
+        setListView(possibleUsersListView, testUsers);
+        setSaldos(testUsers[0]);
+	}
+	
+	private void setListView(ListView listView, String[] list) {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            	android.R.layout.simple_list_item_1, android.R.id.text1, list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
         	public void onItemClick(AdapterView<?> parent, View view,
         		int position, long id) {
         		String item = (String) parent.getAdapter().getItem(position);
@@ -39,8 +41,34 @@ public class ConfirmPurchaseActivity extends Activity {
         		setSaldos(item);
         	}
         }); 
-        
-        //Countdown -code
+	}
+	
+	private void setSaldos(String username) {
+		TextView saldoEspresso = (TextView) findViewById(R.id.saldoEspresso);
+		TextView saldoCoffee = (TextView) findViewById(R.id.saldoCoffee);
+		
+		//TODO: get saldos from client, currently testSaldos used instead.
+		int testSaldoCof = -2;
+		int testSaldoEsp = 4;
+		String newTextForSaldoEspresso = "Espressosaldosi on " + testSaldoCof;
+		// TODO + amount of espresso bought if any
+		String newTextForSaldoCoffee = "Kahvisaldosi on " + testSaldoEsp;
+		// TODO + amount of coffee bought if any
+		saldoCoffee.setText(newTextForSaldoCoffee);
+		saldoEspresso.setText(newTextForSaldoEspresso);
+		
+		if (testSaldoCof >= 0) // TODO: plus amount of coffee bought if any
+			saldoCoffee.setTextColor(Color.GREEN);
+		else
+			saldoCoffee.setTextColor(Color.RED);
+		
+		if (testSaldoEsp >= 0) // TODO: plus amount of espresso bought if any
+			saldoEspresso.setTextColor(Color.GREEN);
+		else
+			saldoEspresso.setTextColor(Color.RED);
+	}
+	
+	private void setCountdown() {
 		final TextView countdown = (TextView) findViewById(R.id.cp_rec_countdown);
 		countdown.setText(getString(R.string.countdown_prefix) + " "
 				+ COUNTDOWN_LENGTH + getString(R.string.countdown_suffix));
@@ -61,31 +89,6 @@ public class ConfirmPurchaseActivity extends Activity {
 			}
 		};
 		cd.start();
-	}
-	
-	private void setSaldos(String username) {
-		TextView saldoEspresso = (TextView) findViewById(R.id.saldoEspresso);
-		TextView saldoCoffee = (TextView) findViewById(R.id.saldoCoffee);
-		
-		//TODO: get saldos from client, currently testSaldos used instead.
-		int testSaldoCof = -2;
-		int testSaldoEsp = 4;
-		String newTextForSaldoEspresso ="" + saldoEspresso.getText() + testSaldoCof;
-		// TODO + amount of espresso bought if any
-		String newTextForSaldoCoffee = "" + saldoCoffee.getText() + testSaldoEsp;
-		// TODO + amount of coffee bought if any
-		saldoCoffee.setText(newTextForSaldoCoffee);
-		saldoEspresso.setText(newTextForSaldoEspresso);
-		
-		if (testSaldoCof >= 0) // TODO: plus amount of coffee bought if any
-			saldoCoffee.setTextColor(Color.GREEN);
-		else
-			saldoCoffee.setTextColor(Color.RED);
-		
-		if (testSaldoEsp >= 0) // TODO: plus amount of espresso bought if any
-			saldoEspresso.setTextColor(Color.GREEN);
-		else
-			saldoEspresso.setTextColor(Color.RED);
 	}
 	
 	public void onCPOkClick(View v) {
