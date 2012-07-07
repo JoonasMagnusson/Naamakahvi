53a745de8c56f8cfe89a2c6d23e1ba5a9f35df87
iimakis <iimakis@KUOLAPALLO.(none)>
2012-07-07 16:54:02 +0300

    confirm_purchase.xml aloitusta

--- Naamakahvi-parent/android-ui/gen/naamakahvi/android/R.java
+++ Naamakahvi-parent/android-ui/gen/naamakahvi/android/R.java
@@ -16,34 +16,37 @@ public final class R {
     public static final class id {
         public static final int button1=0x7f070001;
         public static final int button2=0x7f070002;
-        public static final int button3=0x7f070005;
-        public static final int clientRegisterButton=0x7f07000a;
+        public static final int button3=0x7f070007;
+        public static final int clientRegisterButton=0x7f07000c;
         public static final int cnf_text=0x7f070000;
-        public static final int editTextEtunimi=0x7f070008;
-        public static final int editTextSukunimi=0x7f070009;
-        public static final int editTextUsername=0x7f070007;
-        public static final int faceDetectView1=0x7f07000c;
-        public static final int gridView1=0x7f070011;
-        public static final int imageButton1=0x7f07000b;
-        public static final int lessUnits=0x7f07000e;
-        public static final int moreUnits=0x7f070010;
-        public static final int noOfUnits=0x7f07000f;
+        public static final int editTextEtunimi=0x7f07000a;
+        public static final int editTextSukunimi=0x7f07000b;
+        public static final int editTextUsername=0x7f070009;
+        public static final int faceDetectView1=0x7f07000e;
+        public static final int gridView1=0x7f070013;
+        public static final int imageButton1=0x7f07000d;
+        public static final int lessUnits=0x7f070010;
+        public static final int moreUnits=0x7f070012;
+        public static final int noOfUnits=0x7f070011;
         public static final int rec_countdown=0x7f070003;
-        public static final int regButton=0x7f070012;
-        public static final int textView1=0x7f070006;
-        public static final int textView2=0x7f07000d;
-        public static final int userListView=0x7f070004;
+        public static final int regButton=0x7f070014;
+        public static final int saldoEspresso=0x7f070005;
+        public static final int saldoKahvi=0x7f070004;
+        public static final int textView1=0x7f070008;
+        public static final int textView2=0x7f07000f;
+        public static final int userListView=0x7f070006;
     }
     public static final class integer {
         public static final int MAX_FASTORDER_UNITS=0x7f050000;
     }
     public static final class layout {
         public static final int confirm=0x7f030000;
-        public static final int custom_order=0x7f030001;
-        public static final int loginwithusername=0x7f030002;
-        public static final int menu=0x7f030003;
-        public static final int new_user=0x7f030004;
-        public static final int recog_user=0x7f030005;
+        public static final int confirm_purchase=0x7f030001;
+        public static final int custom_order=0x7f030002;
+        public static final int loginwithusername=0x7f030003;
+        public static final int menu=0x7f030004;
+        public static final int new_user=0x7f030005;
+        public static final int recog_user=0x7f030006;
     }
     public static final class raw {
         public static final int lbpcascade_frontalface=0x7f040000;
