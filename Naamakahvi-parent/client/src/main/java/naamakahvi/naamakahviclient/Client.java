4b1b84417d176c9bb6efdd1195c6b56d48d92e75
Janne Ronkonen <leadnose@gmail.com>
2012-07-10 11:21:53 +0300

    Poistettu URIBuilderin käyttö ja rakennetaan URIt käsin.

--- Naamakahvi-parent/client/src/main/java/naamakahvi/naamakahviclient/Client.java
+++ Naamakahvi-parent/client/src/main/java/naamakahvi/naamakahviclient/Client.java
@@ -8,15 +8,12 @@ import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
@@ -76,7 +73,7 @@ public class Client {
     }
 
     private URI buildURI(String path) throws Exception {
-        return new URIBuilder().setScheme("http").setHost(this.host).setPort(this.port).setPath(path).build();
+        return new URI("http://" + this.host + ":" + this.port + path);
     }
 
     private JsonObject doPost(String path, String... params) throws Exception {
@@ -113,13 +110,13 @@ public class Client {
 
         final HttpClient c = new DefaultHttpClient();
 
-        final URIBuilder ub = new URIBuilder().setScheme("http").setHost(this.host).setPort(this.port).setPath(path);
+        String suri = "http://" + this.host + ":" + this.port + path + "?";
 
         for (int i = 0; i < params.length; i += 2) {
-            ub.setParameter(params[i], params[i + 1]);
+            suri = suri + params[i] + "=" + params[i+1] + "&";
         }
 
-        final URI uri = ub.build();
+        final URI uri = new URI(suri);
         final HttpGet get = new HttpGet(uri);
 
         get.addHeader("Content-Type", "application/x-www-form-urlencoded");
