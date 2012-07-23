e41d0225da08d8898058c269cfb81f0a795cd053
Samir Puuska <puuska@cs.helsinki.fi>
2012-07-23 11:35:42 +0300

    bugfix

--- Python/server.py
+++ Python/server.py
@@ -72,6 +72,7 @@ def upload():
         if file:
             print file
             file.save(secure_filename(file.filename))
+            cvm.train(secure_filename(file.filename))
             return resp_ok()
         else:
             return resp_failure('NoFileInRequestError')
