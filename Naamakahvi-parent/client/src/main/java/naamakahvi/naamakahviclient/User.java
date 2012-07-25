bc8435a65fa861f2a7a6682a5ce1b86937a770a5
Eeva Terkki <juuuro@gmail.com>
2012-07-25 11:45:35 +0300

    Balance User-luokkaan

--- Naamakahvi-parent/client/src/main/java/naamakahvi/naamakahviclient/User.java
+++ Naamakahvi-parent/client/src/main/java/naamakahvi/naamakahviclient/User.java
@@ -1,14 +1,18 @@
 package naamakahvi.naamakahviclient;
 
+import java.util.List;
+
 class User implements IUser {
     private String username;
     private String givenName;
     private String familyName;
+    private List<SaldoItem> balance;
 
-    User(String username, String givenName, String familyName) {
+    User(String username, String givenName, String familyName, List<SaldoItem> balance) {
         this.username = username;
         this.givenName = givenName;
         this.familyName = familyName;
+        this.balance = balance;
     }
 
     public String getUserName() {
@@ -26,5 +30,9 @@ class User implements IUser {
 
     public String getGivenName() {
         return givenName;
+    }
+
+    public List<SaldoItem> getBalance() {
+        return balance;
     }
 }
